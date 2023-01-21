cc8a34ccb6ec8730af94a806401026f1b6548b35
reiikennedy <mutuakennedy81@gmail.com>
2023-01-22 01:52:19 +0300

    reorder commands, add gunicorn proxy bind command

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,6 @@ ls -la /vol/web
 
 whoami
 
-python manage.py collectstatic --noinput
 python manage.py migrate
-
-gunicorn --bind :8000 --workers 2 homey.wsgi
+python manage.py collectstatic --noinput
+gunicorn homey.wsgi:application --bind 0.0.0.0:8000 --workers 2 
